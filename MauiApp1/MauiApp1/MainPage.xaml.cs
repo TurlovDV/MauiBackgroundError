261509decd28b4d25ba34a84ba00ee50186f43bf
TurlovDV <124359442+TurlovDV@users.noreply.github.com>
2023-03-08 13:43:36 +0300

    com

---
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Java.Security;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+
+	public IServicesTest serviceTest;
+	public MainPage(IServicesTest Services_)
+	{
+		InitializeComponent();
+		serviceTest = Services_;
+	}
+
+	bool isStart;
+    private int i;
+
+    private void OnCounterClicked(object sender, EventArgs e)
+	{
+		CounterBtn.Text = i++.ToString();
+		isStart = !isStart;
+		if (isStart)
+			serviceTest.Start();
+		else
+			serviceTest.Stop();
+			
+	}
+}
+
